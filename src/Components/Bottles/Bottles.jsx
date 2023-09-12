6f3b230e3290c46fed7280f41b56f0be42604a83
joykbiswas <joykumarbiswas100@gmail.com>
2023-09-13 01:18:43 +0600

    remove item added

--- src/Components/Bottles/Bottles.jsx
+++ src/Components/Bottles/Bottles.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useState } from "react";
 import Bottle from "../Bottle/Bottle";
 import "./Bottles.css"
-import { addToLS, getStoredCart } from "../../Utility/LocalStorage";
+import { addToLS, getStoredCart, removeFromLS } from "../../Utility/LocalStorage";
 import Cart from "../Cart/Cart";
 
 const Bottles = () => {
@@ -14,6 +14,8 @@ const Bottles = () => {
         .then(res =>res.json())
         .then(data =>setBottles(data))
     },[])
+
+    
 
     // load cart from local store
     useEffect(()=>{
@@ -38,18 +40,28 @@ const Bottles = () => {
     },[bottles])
 
     const handleAddToCart =bottle=>{
-    //    console.log(bottle);
-    const newCart =[...cart, bottle]
-    setCart(newCart)
-    addToLS(bottle.id)
-    
+        //    console.log(bottle);
+        const newCart =[...cart, bottle]
+        setCart(newCart)
+        addToLS(bottle.id)
+        
     }
+
+    const handleRemoveFromCart= id =>{
+        const remainingCart= cart.filter(bottle=>bottle.id !==id)
+        setCart(remainingCart)
+        removeFromLS (id)
+    }
+
+   
 
     return (
         <div>
             <h3>Bottles here : {bottles.length}</h3>
              
-             <Cart cart={cart}></Cart>
+             <Cart cart={cart}
+             handleRemoveFromCart={handleRemoveFromCart}
+             ></Cart>
             <div className="bottle-container">
               {
                 bottles.map(bottle=> <Bottle 
--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,13 +1,17 @@
 import PropTypes from 'prop-types';
 import "./Cart.css"
-const Cart = ({cart}) => {
+const Cart = ({cart,handleRemoveFromCart}) => {
     return (
         <div>
             <h3>Cart: {cart.length}</h3>
             <div className="cart-container">
                 {
-                    cart.map(bottle=> <img key={bottle.id} src={bottle.img}></img>)
+                    cart.map(bottle=> <div key={bottle.id}>
+                        <img  src={bottle.img}></img>
+                        <button onClick={()=>handleRemoveFromCart(bottle.id)}>Remove</button>
+                    </div>)
                 }
+                
             </div>
         </div>
     );
@@ -15,6 +19,7 @@ const Cart = ({cart}) => {
 
 Cart.propTypes={
     cart:PropTypes.array.isRequired,
+    handleRemoveFromCart:PropTypes.func.isRequired
 }
 
 export default Cart;
